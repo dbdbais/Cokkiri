e4fdd414f3a66deb15f8e217fc62535085fd3096
어지민 <djwlalssla@naver.com>
2024-07-19 17:28:02 +0900

    스터디 게시판 CSS 추가

--- vue-project1/src/components/board/StudyDetail.vue
+++ vue-project1/src/components/board/StudyDetail.vue
@@ -11,8 +11,14 @@ defineProps({
     <div class="detail-content">
       <RecruitmentBadge class="badge" />
       <div class="title group-name">{{ detailData.groupName }}</div>
-      <div class="top"></div>
-      <div class="middle"></div>
+      <div class="top">
+        <div class="box-sb lang"></div>
+        <div class="box-sb member"></div>
+      </div>
+      <div class="middle">
+        <div class="box-sb time"></div>
+        <div class="box-sb rules"></div>
+      </div>
       <div class="bottom"></div>
       <button class="content board-btn">가입신청</button>
     </div>
@@ -20,9 +26,16 @@ defineProps({
 </template>
 
 <style scoped>
+.top,
+.middle,
+.bottom {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .top {
   height: 70px;
-  background-color: yellow;
+  background-color: transparent;
 }
 .middle {
   height: 230px;
@@ -32,14 +45,27 @@ defineProps({
   height: 200px;
   background-color: gray;
 }
+.lang {
+  width: 20%;
 
+  height: 50px;
+}
+.member {
+  width: 70%;
+  height: 50px;
+}
+.time,
+.rules {
+  width: 49%;
+  height: 230px;
+}
 .study-detail {
   width: 700px;
   height: 700px;
   z-index: 1;
   position: absolute;
   top: 5px;
-  left: 325px;
+  left: 350px;
 
   display: flex;
   justify-content: center;
@@ -53,7 +79,7 @@ defineProps({
   margin-top: 70px;
   width: 620px;
   height: 300px;
-  background-color: red;
+  background-color: transparent;
 }
 .board-btn {
   background-color: #ff4d4d;

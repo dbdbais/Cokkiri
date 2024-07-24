1cd940aade9cb8e20c62bbb9c2b27e1522e84b39
이강우 <rain5191@naver.com>
2024-07-24 11:06:16 +0900

    build: 초기 설정

---
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/IscodeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.iscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscodeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IscodeApplication.class, args);
+	}
+}
---
+++ be-spring/iscode/iscode/src/test/java/com/ssafy/iscode/IscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.iscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

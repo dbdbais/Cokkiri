8571ddf60fde653613e6fced67b6a8577d973f39
이강우 <rain5191@naver.com>
2024-07-24 14:35:59 +0900

    feat: 웹 크롤러 기능 구현 완료

---
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/util/WebCrawler.java
@@ -0,0 +1,62 @@
+package com.ssafy.iscode.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class WebCrawler {
+
+    //GET Problem Information of BaekJoon OJS
+    public String getProblemInfo(Long pid) throws IOException {
+        //Crawl Problem info by pid
+
+        String url = "https://www.acmicpc.net/problem/";
+        //String problemName = "21611";
+        Document document = Jsoup.connect(url+pid).get();
+        //
+
+        Elements elem = document.select("#problem-body");
+
+        // delete button element & hint
+        elem.select("button").remove();
+        elem.select("#hint").remove();
+        // change all a tag to p tag
+        for (Element anchor : elem.select("a")) {
+            Element p = new Element("span").text(anchor.text());
+            anchor.replaceWith(p);
+        }
+        // return element to HTML
+        return elem.html();
+    }
+    public static void main(String[] args) throws IOException {
+
+        //for Test
+        
+        String url = "https://www.acmicpc.net/problem/";
+        // problem url
+        String problemName = "21611";
+        // problem name
+        Document document = Jsoup.connect(url+problemName).get();
+
+        Elements elem = document.select("#problem-body");
+
+        // delete button element & hint
+        elem.select("button").remove();
+        elem.select("#hint").remove();
+        // change all a tag to p tag
+        for (Element anchor : elem.select("a")) {
+            Element p = new Element("span").text(anchor.text());
+            anchor.replaceWith(p);
+        }
+
+        //print element
+        System.out.println(elem);
+
+    }
+}

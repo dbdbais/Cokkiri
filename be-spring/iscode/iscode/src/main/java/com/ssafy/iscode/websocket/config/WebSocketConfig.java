55e85a6880230c6a7f87a74d4f8f3db5fe57c2eb
김영수 <kimyeong3732@naver.com>
2024-07-29 14:56:25 +0900

    chore: 웹 소켓 기능 구현 - 메세지 소켓 : 메세지 전송 및 저장, 다중 방 구현 - 로비 소켓 : 알림 기능 구현 - 대기방 소켓 : 유저 입퇴장, 준비 및 시작 이벤트 구현, 채팅기능 구현

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/websocket/config/WebSocketConfig.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/websocket/config/WebSocketConfig.java
@@ -3,18 +3,44 @@ package com.ssafy.iscode.websocket.config;
 import com.ssafy.iscode.websocket.handler.ChatWebSocketHandler;
 import com.ssafy.iscode.websocket.handler.LobbyWebSocketHandler;
 import com.ssafy.iscode.websocket.handler.RoomWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatWebSocketHandler(), "/chat/{roomId}").setAllowedOrigins("*");
-        registry.addHandler(new LobbyWebSocketHandler(), "/lobby").setAllowedOrigins("*");
-        registry.addHandler(new RoomWebSocketHandler(), "/room/{roomId}").setAllowedOrigins("*");
+        // message socket
+        registry.addHandler(ChatWebSocketHandler(), "/chat/{roomId}/{userName}")
+                .setAllowedOrigins("*");
+
+        // lobby socket
+        registry.addHandler(LobbyWebSocketHandler(), "/lobby")
+                .setAllowedOrigins("*");
+
+        // room socket
+        registry.addHandler(RoomWebSocketHandler(), "/room/{roomId}/{userName}")
+                .setAllowedOrigins("*");
+    }
+
+    // to resolve a bug
+    @Bean
+    public ChatWebSocketHandler ChatWebSocketHandler() {
+        return new ChatWebSocketHandler();
+    }
+
+    @Bean
+    public LobbyWebSocketHandler LobbyWebSocketHandler() {
+        return new LobbyWebSocketHandler();
+    }
+
+    @Bean
+    public RoomWebSocketHandler RoomWebSocketHandler() {
+        return new RoomWebSocketHandler();
     }
 }
--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/websocket/handler/RoomWebSocketHandler.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/websocket/handler/RoomWebSocketHandler.java
@@ -1,6 +1,115 @@
 package com.ssafy.iscode.websocket.handler;
 
+import com.ssafy.iscode.user.model.dao.UserRepository;
+import com.ssafy.iscode.user.model.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.util.UriTemplate;
+
+import java.util.*;
 
 public class RoomWebSocketHandler extends TextWebSocketHandler {
+    private final Map<String, Set<WebSocketSession>> roomSessions = Collections.synchronizedMap(new HashMap<>());
+    private final Map<WebSocketSession, String> userSessions = Collections.synchronizedMap(new HashMap<>());
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // connected
+    // room id, user name manage
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        String roomId = getRoomId(session);
+        String userName = getUserName(session);
+
+        roomSessions.computeIfAbsent(roomId, k -> Collections.synchronizedSet(new HashSet<>())).add(session);
+        userSessions.put(session, userName);
+
+        // send user enter event
+        String event = ".|!|.|!|ENTER|!|" + userName;
+        sendEvent(roomId, event);
+    }
+
+    // send message to all user in room
+    // separator = |!|
+    // content : user name |!| message |!| event name |!| event parameter
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String roomId = getRoomId(session);
+        String userName = userSessions.get(session);
+        String m = message.getPayload();
+        String content = userName + "|!|" + m;
+
+        // press start button
+        if ("|@|".equals(m)) {
+            String event = ".|!|.|!|START|!|.";
+            sendEvent(roomId, event);
+        } else if ("|#|".equals(m)) { // press ready button
+            String event = ".|!|.|!|.READY|!|" + userName;
+            sendEvent(roomId, event);
+        } else if ("|$|".equals(m)) { // press not ready button
+            String event = ".|!|.|!|NREADY|!|" + userName;
+            sendEvent(roomId, event);
+        } else { // just chatting
+            Set<WebSocketSession> sessions = roomSessions.get(roomId);
+            if (sessions != null) {
+                for (WebSocketSession webSocketSession : sessions) {
+                    webSocketSession.sendMessage(new TextMessage(content));
+                }
+            }
+        }
+    }
+
+    // disconnected
+    // room id, user name manage
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        String roomId = getRoomId(session);
+        String userName = getUserName(session);
+        userSessions.remove(session);
+
+        Set<WebSocketSession> sessions = roomSessions.get(roomId);
+        if (sessions != null) {
+            sessions.remove(session);
+            if (sessions.isEmpty()) {
+                roomSessions.remove(roomId);
+
+                // send user quit event
+                String event = ".|!|.|!|QUIT|!|" + userName;
+                sendEvent(roomId, event);
+            }
+        }
+    }
+
+    private String getRoomId(WebSocketSession session) {
+        UriTemplate template = new UriTemplate("/room/{roomId}/{userName}");
+        Map<String, String> variables = template.match(session.getUri().getPath());
+        return variables.get("roomId");
+    }
+
+    private String getUserName(WebSocketSession session) {
+        UriTemplate template = new UriTemplate("/room/{roomId}/{userName}");
+        Map<String, String> variables = template.match(session.getUri().getPath());
+        Optional<User> user = userRepository.findByName(variables.get("userName"));
+
+        if (user.isPresent()) {
+            return user.get().getNickname();
+        }
+
+        throw new RuntimeException("Unknown user");
+    }
+
+    // send event
+    // server -> client
+    private void sendEvent(String roomId, String event) throws Exception {
+        Set<WebSocketSession> sessions = roomSessions.get(roomId);
+        if (sessions != null) {
+            for (WebSocketSession webSocketSession : sessions) {
+                webSocketSession.sendMessage(new TextMessage(event));
+            }
+        }
+    }
 }

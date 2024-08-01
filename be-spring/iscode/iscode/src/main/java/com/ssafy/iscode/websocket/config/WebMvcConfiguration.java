5ca176d1e9f32d092875dd43b2f54d89b354c2a3
준수 <skymoon28@nate.com>
2024-08-01 17:48:30 +0900

    feat: cors

---
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/websocket/config/WebMvcConfiguration.java
@@ -0,0 +1,27 @@
+package com.ssafy.iscode.websocket.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@MapperScan(basePackages = {"com.ssafy.iscode.*.model.dao"})
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:5173")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.OPTIONS.name())
+                .maxAge(1800);
+    }
+}
+

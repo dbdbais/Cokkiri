0de0fdca7dd11048dfac26c478a56349b6d8c3bb
어지민 <djwlalssla@naver.com>
2024-08-01 11:07:43 +0900

    feat: ing 문제목록 페이지 레이아웃 작업중

---
+++ fe-vue/src/components/problem/ProblemItem.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  problemData: Object,
+});
+
+const tierImg = "/src/assets/tier/";
+</script>
+
+<template>
+  <tr class="bold-text">
+    <td>{{ problemData.no }}</td>
+    <td>
+      {{ problemData.problemName }}
+    </td>
+    <td>
+      <img
+        :src="
+          tierImg +
+          problemData.difficulty.tier +
+          '-' +
+          problemData.difficulty.level +
+          '.svg'
+        "
+        alt="티어"
+      />
+    </td>
+    <td>{{ problemData.answerPercentage }}</td>
+    <td v-if="problemData.review.result === 1">리뷰보기</td>
+    <td v-else-if="problemData.review.try >= 3">리뷰보기</td>
+    <td v-else>{{ problemData.review.try }} / 3 회 도전</td>
+  </tr>
+</template>
+
+<style scoped>
+.bold-text {
+  font-size: 25px;
+  -webkit-text-stroke: 1.5px black;
+}
+td {
+  /* padding: 1em 0.5em; */
+  padding: 10px;
+  vertical-align: middle;
+}
+
+td {
+  text-align: center;
+  border: 3px solid #3b72ff;
+  background: #bed0fd;
+}
+</style>
--- fe-vue/src/components/problem/ProblemList.vue
+++ fe-vue/src/components/problem/ProblemList.vue
@@ -1,11 +1,89 @@
 <script setup>
+import ProblemItem from "@/components/problem/ProblemItem.vue";
+import { ref } from "vue";
 
+const problemList = ref([
+  {
+    id: 1,
+    no: 21,
+    problemName: "단어 뒤집기",
+    answerPercentage: "24%",
+    difficulty: { tier: "gold", level: 4 },
+    review: { try: 2, result: 1 },
+  },
+  {
+    id: 2,
+    no: 23,
+    problemName: "종덕 뒤집기",
+    answerPercentage: "33%",
+    difficulty: { tier: "gold", level: 4 },
+    review: { try: 3, result: 0 },
+  },
+  {
+    id: 3,
+    no: 27,
+    problemName: "뒤집기",
+    answerPercentage: "64%",
+    difficulty: { tier: "silver", level: 4 },
+    review: { try: 1, result: 0 },
+  },
+]);
 </script>
 
 <template>
-    <div>
-        ProblemList
-    </div>
+  <div>
+    <table>
+      <thead class="bold-text">
+        <tr>
+          <th>No</th>
+          <th>문제</th>
+          <th>난이도</th>
+          <th>정답률</th>
+          <th>리뷰 보기</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProblemItem
+          v-for="problem in problemList"
+          :key="problem.id"
+          :problem-data="problem"
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+  padding: 1.5em;
+  background: #f5f5f5;
+}
+
+table {
+  border: 1px #a39485 solid;
+  font-size: 0.9em;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25);
+  width: 100%;
+  border-collapse: collapse;
+  border-radius: 5px;
+  overflow: hidden;
+
+  margin-top: 20px;
+}
+th {
+  padding: 1em 0.5em;
+  vertical-align: middle;
+  text-align: center;
+  border-right: 3px solid white;
+}
+
+thead {
+  font-size: 25px;
+  color: #fff;
+  background: #5b89ff;
+}
+
+a {
+  color: #73685d;
+}
+</style>

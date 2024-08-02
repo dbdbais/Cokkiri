b986273176e2ec6fbeb784f90e8507be75e05ddc
이준수 <skymoon28@nate.com>
2024-08-02 09:47:24 +0900

    feat: add router (start, user, registry)

--- fe-vue/src/router/index.js
+++ fe-vue/src/router/index.js
@@ -8,6 +8,21 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/start",
+      name: "start",
+      component: () => import("@/views/StartView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/LoginView.vue"),
+    },
+    {
+      path: "/registry",
+      name: "registry",
+      component: () => import("@/views/RegistryView.vue"),
+    },
+    {
       path: "/",
       name: "home",
       component: HomeView,
---
+++ fe-vue/src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <RouterLink :to="{ name: 'registry' }">
+            <div class="registry">
+                <span>처음이신가요? 회원가입</span>
+            </div>
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped></style>

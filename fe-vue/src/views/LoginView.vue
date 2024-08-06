de557929996f8b72f213fa1e186f807c5b81e2b6
김종덕 <kjd742388@gmail.com>
2024-08-06 14:53:50 +0900

    feat: 로그인, 화원가입 페이지 수정

--- fe-vue/src/views/LoginView.vue
+++ fe-vue/src/views/LoginView.vue
@@ -1,43 +1,8 @@
-<template>
-  <div class="login-container">
-    <div class="background" :style="{ left: position + 'px' }"></div>
-    <div class="login-box">
-      <img id="elephant" src="@/assets/login_elephant.svg" alt="" />
-      <form @submit.prevent="submitForm">
-        <div class="form-group1">
-          <label for="username">아이디 :</label>
-          <input type="text" id="username" v-model="userData.id" />
-        </div>
-
-        <div class="form-group2">
-          <label for="password">비밀번호 :</label>
-          <input type="password" id="password" v-model="userData.password" />
-        </div>
-
-        <button type="submit" class="login-button">로그인</button>
-      </form>
-      <RouterLink :to="{ name: 'registry' }">
-        <div class="registry">
-          <span>처음이신가요? 회원가입</span>
-        </div>
-      </RouterLink>
-      <button class="google-login">
-        <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google Logo" />
-        Continue with Google
-      </button>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import "@/assets/css/main.css";
+import "@/assets/css/home.css";
 import { ref } from "vue";
-import { login, getUser } from "@/api/user";
-import { userStore } from "@/stores/user";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-const store = userStore();
+import { login } from "@/api/user";
+import { RouterLink } from "vue-router";
 
 const userData = ref({
   id: "",
@@ -47,103 +12,78 @@ const userData = ref({
 const submitForm = async () => {
   try {
     const response = await login(userData.value);
-    getUserData();
     console.log(response);
   } catch (e) {
     console.log(e);
   }
 };
-
-const getUserData = async () => {
-  try {
-    const response = await getUser(userData.value.id);
-    store.setUser(response.data);
-    console.log(response);
-    router.push({ name: "home" });
-  } catch (e) {
-    console.log(e);
-  }
-}
-// export default {
-//   data() {
-//     return {
-//       username: "",
-//       password: "",
-//     };
-//   },
-//   mounted() {
-//     this.startAnimation();
-//   },
-//   methods: {
-//     handleSubmit() {
-//       // Handle login logic here
-//       console.log("Username:", this.username);
-//       console.log("Password:", this.password);
-//     },
-//     startAnimation() {
-//       const containerWidth = 1000;
-//       const backgroundWidth = 1000; // 배경 이미지의 너비
-//       const resetPosition = -backgroundWidth;
-//       const startPosition = containerWidth;
-
-//       this.position = startPosition;
-
-//       setInterval(() => {
-//         this.position -= 1;
-
-//         if (this.position <= resetPosition) {
-//           this.position = startPosition;
-//         }
-//       }, 16); // 60fps (1000ms/60 ≈ 16ms)
-//     },
-//   },
-// };
 </script>
 
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <form @submit.prevent="submitForm">
+        <img id="elephant" src="@/assets/login_elephant.svg" alt="" />
+        <div class="form-group">
+          <label for="username" class="title" id="id">아이디 </label>
+          <input type="text" id="username" v-model="userData.id" />
+        </div>
+
+        <div class="form-group">
+          <label for="password" class="title">비밀번호 </label>
+          <input type="password" id="password" v-model="userData.password" />
+        </div>
+
+        <button id="submit" type="submit" class="title">로그인</button>
+      </form>
+      <RouterLink :to="{ name: 'registry' }">
+        <a href="#" class="register-link">처음이신가요? 회원가입</a>
+      </RouterLink>
+
+      <button class="google-login">
+        <img
+          src="https://developers.google.com/identity/images/g-logo.png"
+          alt="Google Logo"
+        />
+        <span class="title">Continue with Google</span>
+      </button>
+    </div>
+  </div>
+</template>
+
 <style scoped>
-.background {
-  position: absolute;
-  top: 0;
-  width: 1000px;
-  /* 배경 이미지의 너비와 동일하게 설정 */
-  height: 100vh;
-  background-image: url("@/assets/login_bg_elephant.svg");
-  /* 배경 이미지 경로 */
-  background-size: cover;
+#id {
+  letter-spacing: 2.7vh;
 }
 
-.login-box {
-  position: relative;
+span {
+  color: black;
 }
-
 #elephant {
-  position: absolute;
-  height: 100px;
-  top: -90px;
-  left: 180px;
-  z-index: 1;
+  position: relative;
+  top: -12vh;
+  height: 20vh;
 }
 
 .login-container {
-  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
+  /* background-image: url("@/assets/login_bg_elephant.svg"); */
   background-color: #d1e7ff;
-  background-repeat: no-repeat;
-  background-size: cover;
+  background-repeat: space;
 }
 
 .login-box {
-  border: 5px solid #3b72ff;
   background-color: #a6c9ff;
-  padding: 20px;
-  border-radius: 10px;
+  padding: 3vh;
+  border-radius: 20px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   text-align: center;
-  height: 50vh;
-  width: 50vh;
+  height: 60vh;
+  width: 90vh;
+  border: 10px solid #0073e6;
 }
 
 form {
@@ -151,92 +91,93 @@ form {
   flex-direction: column;
 }
 
-.form-group1 {
-  font-size: 40px;
+.form-group {
   display: flex;
+  position: relative;
   align-items: center;
-  margin-top: 40px;
-  margin-bottom: 20px;
-}
-
-.form-group2 {
-  font-size: 31px;
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
+  margin-bottom: 3vh;
+  font-size: 6vh;
+  top: -10vh;
+  -webkit-text-stroke: 1px black;
 }
 
 label {
   flex: 1;
-  font-weight: normal;
-  text-align: right;
+  font-weight: bold;
+  text-align: left;
   margin-right: 10px;
-  -webkit-text-stroke: 1px black;
+  flex-direction: row;
 }
 
 input {
   flex: 2;
   padding: 8px;
-  border: 1px solid #ccc;
+  border: 5px solid black;
   border-radius: 5px;
-  color: black;
+  font-size: 30px;
+  border-radius: 10px;
 }
 
 button {
-  padding: 5px;
   background-color: #a6a6ff;
   color: white;
   border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  width: 100%;
-  margin-top: 10px;
-  font-weight: normal;
-  -webkit-text-stroke: 0.5px black;
-  margin-bottom: 10px;
-  border: 3px solid #3b72ff;
+  border-radius: 20px;
+  font-size: 4vh;
+  width: 50%;
+  height: 7vh;
+  position: relative;
+  top: -10vh;
+  margin: 0 auto;
+  margin-bottom: 4vh;
+  border: 5px solid #0073e6;
+}
+
+#submit {
+  font-weight: bold;
+  -webkit-text-stroke: 1px black;
 }
 
 button:hover {
   background-color: #8585ff;
-  cursor: url(@/assets/pointer.svg) 2 2, pointer;
 }
 
-.registry {
+.register-link {
+  /* font-weight: bold; */
   display: block;
-  margin-top: 10px;
-  color: #ffffff;
+  position: relative;
+  /* margin-top: 10px; */
   text-decoration: none;
-  font-size: 1em;
+  font-size: 30px;
+  top: -120px;
+  color: white;
 }
 
-.registry:hover {
-  color: #8585ff;
+.register-link:hover {
+  color: #0073e6;
 }
 
 .google-login {
-  margin-top: 20px;
-  margin-bottom: 10px;
   color: black;
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 10px;
+  padding: 2vh;
   background-color: white;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  border: 5px solid #000;
   cursor: pointer;
-  width: 100%;
-  border: 3px solid black;
+  /* margin-top: 10px; */
+  width: 70%;
 }
 
 .google-login img {
-  width: 20px;
-  height: 20px;
+  width: 5vh;
+  height: 5vh;
   margin-right: 10px;
 }
 
-.login-button {
-  font-size: 25px;
+#username,
+#password {
+  height: 6vh;
 }
 </style>

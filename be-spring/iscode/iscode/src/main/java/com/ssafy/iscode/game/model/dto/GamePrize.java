fb27a0e9f338b0769e3d8584794f259a99a3802b
Kangwoo Lee <rain5191@naver.com>
2024-08-07 09:51:17 +0900

    fix: constraint 추가

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/game/model/dto/GamePrize.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/game/model/dto/GamePrize.java
@@ -5,12 +5,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Check;
 
 @Entity
 @Table(name = "game_prize")
 @Getter
 @Setter
 @NoArgsConstructor
+@Check(constraints = "ranking BETWEEN 1 AND 3")
 public class GamePrize {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

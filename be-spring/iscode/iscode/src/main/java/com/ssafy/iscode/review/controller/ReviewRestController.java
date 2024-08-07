6f0bbd46efa7b404aeabae407626712e9b11c04e
김영수 <kimyeong3732@naver.com>
2024-08-07 12:49:04 +0900

    chore: url 적용 안된 부분 수정 및 컴파일러 패키지 제거

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/review/controller/ReviewRestController.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/review/controller/ReviewRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reviewapi")
+@RequestMapping("/api/reviewapi")
 public class ReviewRestController {
 
     private ReviewService reviewService;

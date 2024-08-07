75bdbff63f11c3449ed6307b5b2ec4242c601c04
이준수 <skymoon28@nate.com>
2024-08-07 09:22:52 +0900

    feat: add logout

--- fe-vue/src/stores/user.js
+++ fe-vue/src/stores/user.js
@@ -1,19 +1,36 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 
-export const userStore = defineStore("user", () => {
-  const user = ref({
-    id: "ssafy",
-    nickname: "ssafy",
-    tier: "SEED",
-    percent: 0,
-  });
+export const userStore = defineStore(
+  "user",
+  () => {
+    const user = ref({
+      id: "ssafy",
+      nickname: "ssafy",
+      tier: "SEED",
+      percent: 0,
+    });
 
-  function setUser(newUser) {
-    user.value = newUser;
+    function setUser(newUser) {
+      user.value = newUser;
+    }
+
+    function logout() {
+      user.value = {
+        id: "",
+        nickname: "",
+        tier: "",
+        percent: 0,
+      };
+    }
+
+    return {
+      user,
+      setUser,
+      logout,
+    };
+  },
+  {
+    persist: true,
   }
-  return {
-    user,
-    setUser,
-  };
-});
+);

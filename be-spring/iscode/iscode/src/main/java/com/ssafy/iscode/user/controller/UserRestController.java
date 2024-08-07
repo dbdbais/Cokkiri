c2c6b6bdb400789f6ff4fda8b84f82ebc7c3dbab
어지민 <djwlalssla@naver.com>
2024-08-07 10:01:31 +0900

    fix: user requestmapping url fixed

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/user/controller/UserRestController.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/user/controller/UserRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/userapi")
+@RequestMapping("/api/userapi")
 public class UserRestController {
 
     private UserService userService;

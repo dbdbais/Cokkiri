5d6177b979efddc7b1973dc435638fe7e795dacf
Kangwoo Lee <rain5191@naver.com>
2024-08-07 09:33:52 +0900

    init: 게임 등수 매핑 완료

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/game/model/dto/GameDto.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/game/model/dto/GameDto.java
@@ -2,16 +2,15 @@ package com.ssafy.iscode.game.model.dto;
 
 import com.ssafy.iscode.problem.model.dto.Problem;
 import com.ssafy.iscode.user.model.dto.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Check;
 
 import java.util.List;
+import java.util.Map;
 
 // additional room setting
 @Entity
@@ -20,6 +19,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Check(constraints = "game_diff BETWEEN 1 AND 5 AND game_mode BETWEEN 1 AND 3")
 public class GameDto {
     // game id == waiting room's id
     // => not auto increment
@@ -28,30 +28,31 @@ public class GameDto {
     private Long id;
 
     // level 3 ~ 5 => 3|5
-    @Column(name = "game_diff")
-    private String difficulty;
+    @Column(name = "game_diff",nullable = false)
+    private int difficulty;
 
     // 30 minute => 30 * 60 * 1000 => 3600000
-    @Column(name = "game_time")
+    @Column(name = "game_time",nullable = false)
     private Long maxTime;
 
     // user success times
     // milli-second
     // 123113, 1123331, 2973314
     // => 123113|1123331|2973314
-    @Column(name = "user_times")
-    private String times;
+    @Column(name = "user_times",nullable = true)
+    private Long times;
 
     // 1 : item mode
     // 2 : blind mode
     // 3 : random language
-    @Column(name = "game_mode")
+    @Column(name = "game_mode",nullable = false)
     private int mode;
 
     // user prices => 1st, 2nd, 3rd
     // max length = 3
-    private List<User> prices;
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GamePrize> prizes;
 
-    // selected problems
-    private List<Problem> problems;
+
+    //private List<Problem> problems;
 }
---
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/game/model/dto/GamePrize.java
@@ -0,0 +1,39 @@
+package com.ssafy.iscode.game.model.dto;
+
+import com.ssafy.iscode.user.model.dto.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "game_prize")
+@Getter
+@Setter
+@NoArgsConstructor
+public class GamePrize {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private GameDto game;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "ranking")
+    private int ranking; // rank
+
+    @Override
+    public String toString() {
+        return "GamePrize{" +
+                "id=" + id +
+                ", game=" + game +
+                ", user=" + user +
+                ", rank=" + ranking +
+                '}';
+    }
+}

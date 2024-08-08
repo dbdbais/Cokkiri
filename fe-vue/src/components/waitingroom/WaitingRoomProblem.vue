f18417ab024448d8d676734539253a7fb4f57126
어지민 <djwlalssla@naver.com>
2024-08-08 23:22:38 +0900

    feat: 스터디 대기방 문제 추가 디자인 작업 및 필터 기능 추가

--- fe-vue/src/components/waitingroom/WaitingRoomProblem.vue
+++ fe-vue/src/components/waitingroom/WaitingRoomProblem.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="box problem-container bold-text">
     문제 목록
-    <button class="problem-btn" @click="$emit('open')">선택</button>
+    <button class="problem-btn bold-text" @click="$emit('open')">선택</button>
     <div class="problem-box box box-col text-font"></div>
   </div>
 </template>
@@ -13,15 +13,24 @@
   width: 660px;
   height: 320px;
   padding: 10px 20px;
+  position: relative;
 }
 .problem-box {
   width: 610px;
-  height: 220px;
+  height: 230px;
   margin-top: 10px;
   align-items: center;
 }
 .problem-btn {
   width: 100px;
   height: 40px;
+  position: absolute;
+  right: 20px;
+
+  border-radius: 5px;
+  border-width: 4px;
+  border-radius: 5px;
+  border-color: #3b72ff;
+  background-color: #c7d5f7;
 }
 </style>

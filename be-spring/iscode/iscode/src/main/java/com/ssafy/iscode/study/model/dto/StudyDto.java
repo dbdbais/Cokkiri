f2479b17e75a6247c05220814e696e11e7eaf0e8
김영수 <kimyeong3732@naver.com>
2024-08-09 17:10:07 +0900

    chore: study, game createdTime 제거

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/study/model/dto/StudyDto.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/study/model/dto/StudyDto.java
@@ -8,10 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +16,6 @@ import java.util.List;
 @Table(name = "study")
 @Getter
 @Setter
-@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudyDto {
@@ -68,9 +64,5 @@ public class StudyDto {
             inverseJoinColumns = @JoinColumn(name = "problem_id")
     )
     private List<Problem> problems;
-
-    @CreatedDate
-    @Column(name = "createdTime",columnDefinition = "TIMESTAMP", updatable = false)
-    private LocalDateTime createdTime;
 
 }

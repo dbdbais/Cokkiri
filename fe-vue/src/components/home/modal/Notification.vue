c2b368748f9a763fb3606b4fca8716cbbab84bcd
어지민 <djwlalssla@naver.com>
2024-08-12 09:38:29 +0900

    fix: 우편함 새로고침 수정

--- fe-vue/src/components/home/modal/Notification.vue
+++ fe-vue/src/components/home/modal/Notification.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import announcement from "@/assets/data/announcement.json";
 import notification from "@/assets/data/notification.json";
 import AnnounceDetail from "@/components/home/modal/AnnounceDetail.vue";
@@ -57,6 +57,10 @@ function getNotiData() {
 onMounted(() => {
   getNotiData();
   messageStore.readNoti();
+});
+
+onUnmounted(() => {
+  messageStore.resetRoomInvite();
 });
 
 const router = useRouter();

b436e4ab04e7fdf3404cb97a478c5edf7edc07a1
오성혁 <huggycareer@gmail.com>
2024-08-13 23:53:40 +0900

    fix: 로드밸런싱 요청 url 변경

--- be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/util/APIConnection.java
+++ be-spring/iscode/iscode/src/main/java/com/ssafy/iscode/util/APIConnection.java
@@ -145,8 +145,8 @@ public class APIConnection {
     }
 
     public Map<Integer,String> compileCode(CompileRequestDTO compileRequestDTO,boolean sbmit){
-        // String url = "https://i11e108.p.ssafy.io/compiler/";
-        String url = "http://nginx/";
+        String url = "https://i11e108.p.ssafy.io/compiler/";
+        //String url = "http://nginx/";
         if(sbmit){
             url += "submit";
         }
@@ -195,8 +195,8 @@ public class APIConnection {
         System.out.println("run : 1 or submit : 2");
         int pos = Integer.parseInt(in.readLine());
 
-        // String url = "https://i11e108.p.ssafy.io/compiler/";
-        String url = "http://nginx:8082/";
+        String url = "https://i11e108.p.ssafy.io/compiler/";
+        ///String url = "http://nginx:8082/";
 
         if(pos == 1){
             url += "run";

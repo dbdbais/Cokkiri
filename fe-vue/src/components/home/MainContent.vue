0c579abf132035ed49dc7dc05d2117a195d35648
이준수 <skymoon28@nate.com>
2024-08-14 10:55:28 +0900

    refactor: MainContent.vue 리팩토링

--- fe-vue/src/components/home/MainContent.vue
+++ fe-vue/src/components/home/MainContent.vue
@@ -1,68 +1,39 @@
 <template>
   <div class="main-content box-main-con">
     <div class="box-row">
-      <div
-        class="filter-btn box-main-noti"
-        :class="{ active: categoryObj.all }"
-        @click="$emit('is-game')"
-      >
+      <div class="filter-btn box-main-noti" :class="{ active: categoryObj.all }" @click="$emit('is-game')">
         전체
       </div>
-      <div
-        class="filter-btn box-main-noti"
-        :class="{ active: categoryObj.study }"
-        @click="$emit('is-game', false)"
-      >
+      <div class="filter-btn box-main-noti" :class="{ active: categoryObj.study }" @click="$emit('is-game', false)">
         공부방
       </div>
-      <div
-        class="filter-btn box-main-noti"
-        :class="{ active: categoryObj.game }"
-        @click="$emit('is-game', true)"
-      >
+      <div class="filter-btn box-main-noti" :class="{ active: categoryObj.game }" @click="$emit('is-game', true)">
         게임방
       </div>
     </div>
     <div id="room-container" class="rooms">
-      <Room
-        v-for="room in rooms"
-        :key="room.id"
-        :room="room"
-        @click="$emit('go-room', room.sessionId)"
-      />
+      <Room v-for="room in rooms" :key="room.id" :room="room" @click="$emit('go-room', room.sessionId)" />
     </div>
-    <Page
-      class="page"
-      :current-page="currentPage"
-      @change-page="(motion) => $emit('change-page', motion)"
-    />
+    <Page class="page" :current-page="currentPage" @change-page="(motion) => $emit('change-page', motion)" />
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
 import Room from "./Room.vue";
 import Page from "@/components/common/Page.vue";
-// import roomList from "@/assets/data/roomList.json";
-import roomList from "@/assets/data/roomListAxios.json";
 
 defineProps({
   rooms: Object,
   currentPage: Number,
   categoryObj: Object,
 });
-
-rooms: [
-  { id: 1, name: "알고리즘 고수들방", members: 116 },
-  { id: 2, name: "초보. 매너방요", members: 206 },
-  // Add more rooms here
-];
 </script>
 
 <style scoped>
 .main-content {
   position: relative;
 }
+
 .filter-btn {
   padding: 10px 30px;
   margin-right: 15px;
@@ -76,6 +47,7 @@ rooms: [
   border-color: #eaf3f7;
   background-color: #5bb5d9;
 }
+
 #room-container {
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -83,6 +55,7 @@ rooms: [
   margin-top: 20px;
   gap: 35px;
 }
+
 .page {
   bottom: 10px;
   left: 620px;
